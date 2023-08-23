b48a54e75c6fddf41c993b7b780061e7b5fb5266
MATHEUS FILLYPE FERREIRA DE CARVALHO <B51084@fgv.edu.br>
2023-08-23 07:52:13 -0300

    nada

--- segundo_periodo/linguagens_de_programacao/Aula 05 - 18_08_2023 - Módulos.py
+++ segundo_periodo/linguagens_de_programacao/Aula 05 - 18_08_2023 - Módulos.py
@@ -2,7 +2,8 @@ import sys
 
 sys.path.append("C:\\Users\\B51084\\Aulas_FGV\\segundo_periodo\\linguagens_de_programacao\\modulo")
 sys.path = sys.path[0:10]
-
 print(sys.path)
+
 from modulo.modulo import saudacao
+
 saudacao()
